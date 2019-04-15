455aa46b40b5495745e058b68f6f038576b0dfda
donforleonsenior <donforleonsenior@icloud.com>
2019-04-15 16:29:47 +0700

    First Init

---
+++ BullsEye/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BullsEye
+//
+//  Created by Ray Wenderlich on 6/13/18.
+//  Copyright © 2018 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  @IBAction func showAlert() {
+    
+    let alert = UIAlertController(title: "Halo,  Dunia!", message: "Ini App Pertama saya!", preferredStyle: <#T##UIAlertController.Style#>)
+    
+  }
+  
+}
+
+
+
